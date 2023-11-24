2fc84f2932e6c09d616ea090ca033d7d3f576e91
Dyssling <olle.lundgarden@hotmail.com>
2023-11-24 14:45:12 +0100

    Shopping list exercise finished

---
+++ Exercises/ShoppingList/Program.cs
@@ -0,0 +1,19 @@
+﻿var shoppingList = new List<string>();
+string shoppingItem;
+Console.WriteLine("Nedan kan du lägga till dina produkter i shoppinglistan. Skriv 'exit' när listan är färdig.");
+do
+{
+    Console.Clear();
+    Console.WriteLine("Nedan kan du lägga till dina produkter i shoppinglistan. Skriv 'exit' när listan är färdig.");
+    shoppingList.ForEach(Console.WriteLine);
+    shoppingItem = Console.ReadLine()!;
+
+    if(shoppingItem != "exit")
+        shoppingList.Add(shoppingItem);
+
+} while (shoppingItem != "exit");
+
+Console.Clear();
+Console.WriteLine("Din shoppinglista:");
+shoppingList.ForEach(Console.WriteLine);
+Console.ReadKey();
